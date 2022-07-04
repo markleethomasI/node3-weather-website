4e6eb3e93493ff7e5d5cb474fb89457194f67154
Mark Thomas <markleethomas.esq3@gmail.com>
2022-07-03 19:25:44 -0500

    Added wind speed to forecast function return data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,14 +10,14 @@ const request = require('postman-request')
 //    - Success, pass forecast string for data (same format as from before)
 const forecast = (latitude, longitude, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=384e66b13e7984622e02a4d7cc43dc0b&query=${longitude},${latitude}&units=f`
-    request({ url, json: true }, (error, { body }) => {
+    request({ url, json: true }, (error, { body } = {}) => {
         if (error) {
             callback('Cannot connect to weather stack.')
         } else if (body.error) {
             callback(`Weather Stack Error: ${body.error.info}`)
         } else {
-            const { temperature: temp, precip, feelslike, weather_descriptions } = body.current
-            callback(undefined, `${weather_descriptions[0]} It is currently ${temp} degrees out. It feels like ${feelslike}. It There is a ${precip}% chance of rain`)
+            const { temperature: temp, precip, feelslike, weather_descriptions, wind_speed: windSpeed, wind_dir: windDirection } = body.current
+            callback(undefined, `${weather_descriptions[0]} It is currently ${temp} degrees out. It feels like ${feelslike}. It There is a ${precip}% chance of rain. Wind direction: ${windDirection} at ${windSpeed} mph`)
         }
 
     })
