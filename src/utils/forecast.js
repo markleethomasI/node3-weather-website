9c6a18ec2aa7445b6410ee37b2bb5a3f0f5c769a
Mark Thomas <markleethomas.esq3@gmail.com>
2022-07-03 19:38:36 -0500

    Had challenge comments from class enclosed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,13 +1,4 @@
 const request = require('postman-request')
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
 const forecast = (latitude, longitude, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=384e66b13e7984622e02a4d7cc43dc0b&query=${longitude},${latitude}&units=f`
     request({ url, json: true }, (error, { body } = {}) => {
